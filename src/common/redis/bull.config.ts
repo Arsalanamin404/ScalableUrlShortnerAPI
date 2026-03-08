748ce32278a5ce29bbf41a4b0adc1829eb61c6f4
Arslanamin404 <arsalanrather.dev@gmail.com>
2026-03-08 22:45:44 +0530

    fix bullmq redis config for render

--- src/common/redis/bull.config.ts
+++ src/common/redis/bull.config.ts
@@ -3,10 +3,22 @@ import { ConfigService } from '@nestjs/config';
 
 export const BullConfig = BullModule.forRootAsync({
   inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
-    connection: {
-      host: config.getOrThrow<string>('REDIS_HOST'),
-      port: config.getOrThrow<number>('REDIS_PORT'),
-    },
-  }),
+  useFactory: (config: ConfigService) => {
+    const redisUrl = config.getOrThrow<string>('REDIS_URL');
+
+    if (redisUrl) {
+      return {
+        connection: {
+          url: redisUrl,
+        },
+      };
+    }
+
+    return {
+      connection: {
+        host: config.getOrThrow<string>('REDIS_HOST'),
+        port: config.getOrThrow<number>('REDIS_PORT'),
+      },
+    };
+  },
 });
